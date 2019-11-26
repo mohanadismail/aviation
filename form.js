69b058665ba68c57e8e0e33b2d010f6bbe732599
mohanadismail <57875312+mohanadismail@users.noreply.github.com>
2019-11-26 22:28:04 +0200

    Add files via upload

--- form.js
+++ form.js
@@ -6,15 +6,92 @@ var card3 = document.getElementById("card3");
 var card4 = document.getElementById("card4");
 var back1 = document.getElementById("back1");
 var back2 = document.getElementById("back2");
-ind.addEventListener("click", function()
+var body = document.getElementById("body");
+var x = window.matchMedia("(max-width: 900px)");
+if (x.matches) {
+    body.style.overflow=("visible");
+    ind.addEventListener("click", function()
 {
     card1.style.transform=("translateX(40rem)");
     card1.style.opacity="0";
     card2.style.transform=("translateX(20rem)");
     card2.style.opacity="0";
     card3.style.opacity="0.8";
-    card3.style.transform=("translateY(-30rem)");
+    card3.style.transform=("translateY(-53rem)");
+    body.style.overflow=("visible");
 })
+team.addEventListener("click", function()
+{
+    card1.style.transform=("translateX(-20rem)");
+    card1.style.opacity="0";
+    card2.style.transform=("translateX(-40rem)");
+    card2.style.opacity="0";
+    card4.style.opacity="0.8";
+    card4.style.transform=("translateY(-53rem)");
+    body.style.overflow=("visible");
+})
+back1.addEventListener("click", function()
+{
+    card1.style.transform=("translateX(0rem)");
+    card1.style.opacity="1";
+    card2.style.transform=("translateX(0rem)");
+    card2.style.opacity="1";
+    card3.style.opacity="0";
+    card3.style.transform=("translateY(0rem)");
+})
+
+back2.addEventListener("click", function()
+{
+    card1.style.transform=("translateX(0rem)");
+    card1.style.opacity="1";
+    card2.style.transform=("translateX(0rem)");
+    card2.style.opacity="1";
+    card4.style.opacity="0";
+    card4.style.transform=("translateY(0rem)");
+})
+} else {
+    ind.addEventListener("click", function()
+{
+    card1.style.transform=("translateX(40rem)");
+    card1.style.opacity="0";
+    card2.style.transform=("translateX(20rem)");
+    card2.style.opacity="0";
+    card3.style.opacity="0.8";
+    card3.style.transform=("translateY(-25rem)");
+    body.style.overflow=("visible");
+})
+team.addEventListener("click", function()
+{
+    card1.style.transform=("translateX(-20rem)");
+    card1.style.opacity="0";
+    card2.style.transform=("translateX(-40rem)");
+    card2.style.opacity="0";
+    card4.style.opacity="0.8";
+    card4.style.transform=("translateY(-25rem)");
+    body.style.overflow=("visible");
+})
+back1.addEventListener("click", function()
+{
+    card1.style.transform=("translateX(0rem)");
+    card1.style.opacity="1";
+    card2.style.transform=("translateX(0rem)");
+    card2.style.opacity="1";
+    card3.style.opacity="0";
+    card3.style.transform=("translateY(0rem)");
+    body.style.overflow=("hidden");
+})
+
+back2.addEventListener("click", function()
+{
+    card1.style.transform=("translateX(0rem)");
+    card1.style.opacity="1";
+    card2.style.transform=("translateX(0rem)");
+    card2.style.opacity="1";
+    card4.style.opacity="0";
+    card4.style.transform=("translateY(0rem)");
+    body.style.overflow=("hidden");
+})
+}
 card1.addEventListener("mouseover", function()
 {
     card1.style.transform=("translateY(-2rem)");
@@ -30,31 +107,4 @@ card2.addEventListener("mouseover", function()
 card2.addEventListener("mouseout", function()
 {
     card2.style.transform=("translateY(0rem)");
-})
-back1.addEventListener("click", function()
-{
-    card1.style.transform=("translateX(0rem)");
-    card1.style.opacity="1";
-    card2.style.transform=("translateX(0rem)");
-    card2.style.opacity="1";
-    card3.style.opacity="0";
-    card3.style.transform=("translateY(0rem)");
-})
-team.addEventListener("click", function()
-{
-    card1.style.transform=("translateX(-20rem)");
-    card1.style.opacity="0";
-    card2.style.transform=("translateX(-40rem)");
-    card2.style.opacity="0";
-    card4.style.opacity="0.8";
-    card4.style.transform=("translateY(-25rem)");
-})
-back2.addEventListener("click", function()
-{
-    card1.style.transform=("translateX(0rem)");
-    card1.style.opacity="1";
-    card2.style.transform=("translateX(0rem)");
-    card2.style.opacity="1";
-    card4.style.opacity="0";
-    card4.style.transform=("translateY(0rem)");
 })
